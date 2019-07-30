77e96ad2b5cbb036baa1e18a9e7c36172ca7b81e
siyu <214893130@qq.com>
2019-07-30 17:48:56 +0800

    update MysqlHandler to Singleton Patterns

--- tornado_project/common_utilities/sql/sql_base.py
+++ tornado_project/common_utilities/sql/sql_base.py
@@ -1,12 +1,9 @@
 import peewee_async
 from tornado.options import options
 
-from tornado_project.common_utilities.log import logger_error
-
-
 class MySQLHandler:
-    def __init__(self):
-        try:
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, "_instance"):
             db_dic = dict(
                 host=options['SQL_HOST'],
                 port=options['SQL_POST'],
@@ -15,10 +12,10 @@ class MySQLHandler:
                 max_connections=options['SQL_MAX_CONN'],
                 database=options['SQL_DB_NAME'],
             )
-            self.conn = peewee_async.PooledMySQLDatabase(**db_dic, charset='utf8mb4')
-            self.manager = peewee_async.Manager(self.conn)
-        except AttributeError as e:
-            logger_error.error('AttributeError:%s' % e)
+            cls.conn = peewee_async.PooledMySQLDatabase(**db_dic, charset='utf8mb4')
+            cls.manager = peewee_async.Manager(cls.conn)
+            cls._instance = super().__new__(cls)
+        return cls._instance
 
     def get_manager(self):
         return self.manager
