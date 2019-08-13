d6e6a29ba67c1a93e5355f49639a81c63c60bc27
siyu <214893130@qq.com>
2019-08-13 11:14:40 +0800

    update the way of server start

--- tornado_project/run.py
+++ tornado_project/run.py
@@ -7,59 +7,70 @@ from tornado import httpserver
 from tornado.ioloop import IOLoop
 from tornado.options import options
 
-from tornado_project.common_utilities.log import init_log,logger_info
+from tornado_project.common_utilities.log import init_log, logger_info
 from tornado_project.config import parse_options
 
 
-def set_working_dir():
-    project_dir = os.path.dirname(os.path.abspath(__file__))
-    sys.path.append(project_dir)
-    os.chdir(project_dir)
+class Main:
+    def __init__(self):
+        self.loop = None
+        self.app = None
+        self.initialize()
 
+    def initialize(self):
+        # load settings
+        self.set_working_dir()
+        parse_options()
 
-def make_safely_shutdown(server, io_loop):
-    def stop_handler(*args, **keywords):
+        # create app and update settings
+        # ！！注意，为确保options等初始化操作做完，必须在该位置导入，否则options无法全局使用
+        # 若先import后初始化，会编译路由，导致未初始化的option调用失败
+        from tornado_project.app import make_app
+        self.app = make_app(options.COOKIE_SECRET, options.DEBUG)
+        init_log()
 
-        def shutdown():
-            server.stop()
-            # 根据业务调整该超时时间
-            deadline = time.time() + options.SHUTDOWN_MAX_WAIT_TIME
+        self._get_loop()
 
-            def stop_loop():
-                now = time.time()
-                if now < deadline:
-                    io_loop.add_timeout(now + 1, stop_loop)
-                else:
-                    io_loop.stop()
+    def start_server(self):
+        http_server = httpserver.HTTPServer(self.app)
+        http_server.listen(options.PORT)
+        self.make_safely_shutdown(http_server, self.loop)
+        logger_info.info('server start')
+        self.loop.start()
 
-            stop_loop()
+    def _get_loop(self):
+        if not self.loop:
+            self.loop = IOLoop.instance()
+        return self.loop
 
-        io_loop.add_callback_from_signal(shutdown)
+    def set_working_dir(self):
+        project_dir = os.path.dirname(os.path.abspath(__file__))
+        sys.path.append(project_dir)
+        os.chdir(project_dir)
 
-    # signal.signal(signal.SIGQUIT, stop_handler)
-    signal.signal(signal.SIGTERM, stop_handler)
-    signal.signal(signal.SIGINT, stop_handler)
+    def make_safely_shutdown(self, server, io_loop):
+        def stop_handler(*args, **keywords):
 
+            def shutdown():
+                server.stop()
+                # 根据业务调整该超时时间
+                deadline = time.time() + options.SHUTDOWN_MAX_WAIT_TIME
 
-def start_server():
-    # load settings
-    set_working_dir()
-    parse_options()
-    # create app and update settings
+                def stop_loop():
+                    now = time.time()
+                    if now < deadline:
+                        io_loop.add_timeout(now + 1, stop_loop)
+                    else:
+                        io_loop.stop()
 
-    # ！！注意，为确保options等初始化操作做完，必须在该位置导入，否则options无法全局使用
-    # 若先import后初始化，会编译路由，导致未初始化的option调用失败
-    from tornado_project.app import make_app
-    app = make_app(options.COOKIE_SECRET, options.DEBUG)
-    init_log()
+                stop_loop()
 
-    http_server = httpserver.HTTPServer(app)
-    http_server.listen(options.PORT)
-    io_loop = IOLoop.instance()
-    make_safely_shutdown(http_server, io_loop)
-    logger_info.info('server start')
-    io_loop.start()
+            io_loop.add_callback_from_signal(shutdown)
+
+        # signal.signal(signal.SIGQUIT, stop_handler)
+        signal.signal(signal.SIGTERM, stop_handler)
+        signal.signal(signal.SIGINT, stop_handler)
 
 
 if __name__ == '__main__':
-    start_server()
+    Main().start_server()
